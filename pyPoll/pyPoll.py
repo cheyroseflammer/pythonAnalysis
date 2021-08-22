71fdcd92173beae6e54b8dbec383b08a75b9c802
Chey Rose <cheyroseflammer@gmail.com>
2021-08-22 14:28:26 -0700

    start pypoll

--- pyPoll/pyPoll.py
+++ pyPoll/pyPoll.py
@@ -3,15 +3,34 @@ import csv
 
 filepath = os.path.join = ('Resources/election_data.csv')
 
-# Define functions
-def totalVotes(election_data):
-    voter_id = row[0]
-    county = row[1]
-    candidate = row[2]
+# Setting up variables
+
+
+winner_votes = 0
+winner = "" 
     
     
 
 with open(filepath, newline='', encoding='utf-8') as f:
     poll_csv = csv.reader(f, delimiter=',')
+    # Skip header
+    next(f, None)
+    # Set list amd dict variables
+    total_votes_list = []
+    canidate_list = []
+    canidate_dict = {}
+    # Loops through the csv rows
     for row in poll_csv:
-        print(row)
+        # Find length of votes
+        voters = row[0]
+        canidate = row[2]
+        total_votes_list.append(voters)
+    total_votes = len(total_votes_list)
+    print(total_votes)
+
+            
+
+    
+    
+
+    
